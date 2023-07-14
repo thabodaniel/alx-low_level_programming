3959c6ddac7cd9764f8d881913237a0a7deb230a
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-13 19:53:09 -0700

    Edited code to find type on any machine

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- *Main - Entry point
+ *main - Entry point
  *
  *Return: Always 0 (Sucess)
  */
@@ -12,10 +12,10 @@
 	long long int Typex2Long_Int;
 	float TypeFloat;
 
-	printf("Size of a char: %ld byte(s)\n",sizeof(char));
-	printf("Size of an int: %ld bytes(s) \n",sizeof(int));
-	printf("Size of a long int: %ld bytes(s) \n",sizeof(long int));
-	printf("Size of a long long int: %ld bytes(s) \n",sizeof(long long int));
-	printf("Size of a float: %ld byte(s) \n",sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(TypeChar));
+	printf("Size of an int: %ld bytes(s) \n", sizeof(TypeInt));
+	printf("Size of a long int: %ld bytes(s) \n", sizeof(TypeLongInt));
+	printf("Size of a long long int: %ld bytes(s) \n", sizeof(Typex2Long_Int));
+	printf("Size of a float: %ld byte(s) \n", sizeof(TypeFloat));
 	return (0);
 }
