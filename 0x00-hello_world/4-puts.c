6b9453eafbaf6ddc439d8c554027cd9af2e86fe8
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-13 19:04:02 -0700

    Edited comment to meet betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-
-/*This is the main function of the program,
- *the program prints out a statement using
- *the puts fuction and returns 0*/
+ /**
+  *main - Entry point
+  *
+  *Return: Always 0
+  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
