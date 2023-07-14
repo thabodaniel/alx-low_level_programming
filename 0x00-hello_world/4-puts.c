9463155b906eb7c32bb58aa84dc687ec5c2cda42
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-13 18:40:58 -0700

    Formatted commets according to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*This is the main function of the program, the program prints out a statement using the puts fuction and returns 0*/
+
+/*This is the main function of the program,
+ *the program prints out a statement using
+ *the puts fuction and returns 0*/
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
