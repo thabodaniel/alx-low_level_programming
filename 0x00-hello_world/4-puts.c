3c9164422c4dcc38172f3091bea6cfc49d3695f9
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-13 19:09:33 -0700

    Edited according to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
- /**
-  *main - Entry point
-  *
-  *Return: Always 0
-  */
-
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
