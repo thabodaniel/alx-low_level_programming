3d3a5d797a3228862f5c307887738d958c490288
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-19 16:10:35 -0700

    This fuctions checks for a lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *int _islower(int c)
+ *c checks for
+ *lowerecase character
+ *Returns 1 is lowercase
+ *Returns 0 is otherwise
+ */
+
+int _islower (int c)
+{
+	if ( c >= 'a' && c <= 'z' )
+		return (1);
+	else
+		return (0);
+	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,8 @@ void print_alphabet(void);
 
 /* print 10 times the alphabet in lower case */
 void print_alphabet_x10(void);
+
+/* checks for lowercase character */
+int _islower(int c);
+
 #endif
