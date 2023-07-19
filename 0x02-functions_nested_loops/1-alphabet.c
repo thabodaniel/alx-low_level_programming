dab6f4ba1d725365b2871a329f96b66402fce9d5
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-18 17:42:49 -0700

    Wrote a fucntion that prints alphabets in lower case followed by new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_aphabet:
+  *entry point
+  *prints alphabets
+  *in lower case
+  *Return: Always 0(succes)
+  */
+
+void print_alphabet(void)
+{
+	char i;
+	while ( i != 'z' )
+	{
+		_putchar++;
+	}
+	_putchar(/n);
+	return (0);
+}
