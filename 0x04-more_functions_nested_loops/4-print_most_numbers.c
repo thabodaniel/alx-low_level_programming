e2affe4c08dbaac7341b1ff2994b69541196eacc
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-20 16:05:04 -0700

    Modified code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
-			_putchar (i + '0');
-		i++
+		{
+			_putchar ('0' + i);
+		}
 	}
 	_putchar ("\n");
 }
