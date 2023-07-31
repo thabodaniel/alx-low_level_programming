a1e692c0b55bc08a08e37bd135a89e78fc77695c
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-31 16:08:53 -0700

    This fucntion copies the memorry area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,7 @@ int _putchar (char);
 
 /* fill memory with a constant */
 char *_memset(char *s, char b, unsigned int n);
+
+/* copy memory area */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
