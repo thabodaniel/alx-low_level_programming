3cc703b69b418024a86c8a58eff262956328009e
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-07-31 16:34:39 -0700

    this function prints a chesbsboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -20,4 +20,7 @@ char *_strpbrk(char *s, char *accept);
 
 /* locate a substring */
 char *_strstr(char *haystack, char *needle);
+
+/* print a chessboard */
+void print_chessboard(char (*a)[8]);
 #endif
