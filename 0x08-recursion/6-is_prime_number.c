6fc8b5013e74a0b52316b69ddaed857d8caa01d3
Thabo Sehlabo <t.d.sehlabo@gmail.com>
2023-08-03 17:01:26 -0700

    Modified code

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n - 1));
+	return (exact_prime(n, n - 1));
 }
 
 /**
@@ -26,7 +26,7 @@ int exact_prime(int j, int i)
 {
 	if (i == 1)
 		return (1);
-	if (n % i == 0 && i > 0)
+	if (j % i == 0 && i > 0)
 		return (0);
-	return (actual_prime(n, i - 1));
+	return (exact_prime(j, i - 1));
 }
